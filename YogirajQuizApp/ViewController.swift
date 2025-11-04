26f701aa80c117c3cfb16f606a3268bd63e0f6bd
miku1113 <mihirjariwala334@gmail.com>
2025-11-04 23:58:08 +0530

    Initial Commit

---
+++ YogirajQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YogirajQuizApp
+//
+//  Created by mihir jariwala on 04/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
